564d363efb09c4e6b349f9485b98119c87dd44e1
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-12-14 19:31:37 +0200

    Create init.py

---
+++ src/models/init.py
@@ -0,0 +1,9 @@
+"""
+Machine Learning Models for Horse Racing Prediction
+"""
+
+from .predictor import HorseRacingPredictor
+from .model_trainer import ModelTrainer
+from .ensemble_model import AdvancedEnsemble
+
+__all__ = ['HorseRacingPredictor', 'ModelTrainer', 'AdvancedEnsemble']
