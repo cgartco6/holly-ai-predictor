f5df24f052b36b00aa0d90fe4f4323efe18534cb
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-12-14 19:31:02 +0200

    Create init.py

---
+++ src/scraper/init.py
@@ -0,0 +1,8 @@
+"""
+Web Scraping Module for Hollywoodbets
+"""
+
+from .hollywoodbets_scraper import HollywoodBetsScraper
+from .data_cleaner import DataCleaner
+
+__all__ = ['HollywoodBetsScraper', 'DataCleaner']
