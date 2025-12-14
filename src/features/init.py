73d5e2b6edb724588ea497064a1724657b4342d0
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-12-14 19:32:13 +0200

    Create init.py

---
+++ src/features/init.py
@@ -0,0 +1,8 @@
+"""
+Feature Engineering and Analysis Module
+"""
+
+from .feature_engineering import FeatureEngineer
+from .tipster_analyzer import TipsterAnalyzer
+
+__all__ = ['FeatureEngineer', 'TipsterAnalyzer']
